a400f84a58886575fcf787c84dbd8b035127e336
Diac Eugen Codrin <codrin1999@yahoo.com>
2020-04-23 19:07:00 +0300

    Add files via upload

--- lab6pb6.sh
+++ lab6pb6.sh
@@ -1,11 +1,11 @@
-
+#! /bin/bash
 
 folder=$1
+tmpfile=$(mktemp)
+tmpfile2=$(mktemp)
 
-for file in "$folder"/*
-do
-  echo $file
-  NAME=`echo "$file" | cut -d'.' -f1`
-  set -- $NAME*
-  echo "$#"
-done
+
+find $folder -printf "%f\n" | uniq -c > ${tmpfile}
+sed -i 's/^ *//' ${tmpfile} #delete the space in front of the first word of every line
+cut -f 2- -d ' ' ${tmpfile} > ${tmpfile2} 
+sort ${tmpfile2} | uniq --count
